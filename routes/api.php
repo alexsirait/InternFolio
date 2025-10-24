2972cbd6ef0b9933760e6fd5852b88565d4061bf
ALEX SIRAIT <alexsirait1001@gmail.com>
2025-10-24 16:55:46 +0700

    Feat: Setup API
    - Setup API
    - Make API Controller

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\InternController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use function Pest\Laravel\json;
+
+Route::prefix('v1')->group(function () {
+    // Intern
+    Route::prefix('intern')->group(function () {
+        Route::get('/dashboard', [InternController::class, 'dashboard']);
+    });
+});
